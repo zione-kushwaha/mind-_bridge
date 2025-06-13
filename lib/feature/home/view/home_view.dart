a57f1de07d229b1bb9774f3e20c66d208078403c
Jeevan kumar kushwaha <jeevankushwaha461@gmail.com>
2025-06-13 13:01:31 +0545

    working on interface

---
+++ lib/feature/home/view/home_view.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'widgets/templete_widget.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.green,
+          title: Text('Mind Bridge',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold)),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.notifications, color: Colors.white),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            <Widget>[
+              templateWidget(),
+              Container(),
+              Container(),
+            ][currentPageIndex],
+          ],
+        ),
+        bottomNavigationBar: buildBottomNavigation(),
+      );
+
+  Widget buildBottomNavigation() => ConvexAppBar(
+        style: TabStyle.react,
+        shadowColor: Colors.black,
+        backgroundColor: Colors.green,
+        items: [
+          TabItem(icon: FontAwesomeIcons.home, title: ''),
+          TabItem(icon: FontAwesomeIcons.gamepad, title: ''),
+          TabItem(icon: FontAwesomeIcons.user, title: ''),
+        ],
+        initialActiveIndex: currentPageIndex,
+        onTap: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+      );
+}
+//
---
+++ lib/feature/home/view/widgets/templete_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../data/image_data_data.dart';
+import 'image_data_widget.dart';
+
+class templateWidget extends StatefulWidget {
+  @override
+  _templateWidgetState createState() => _templateWidgetState();
+}
+
+class _templateWidgetState extends State<templateWidget> {
+  final pageController = PageController(viewportFraction: 0.8);
+  int pageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) => Column(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.07,
+                    ),
+                    Text(
+                      'Hi, Jeevan Kushwaha',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.63,
+            // color: Colors.red,
+            child: PageView.builder(
+              controller: pageController,
+              itemCount: locations.length,
+              itemBuilder: (context, index) {
+                final location = locations[index];
+
+                return LocationWidget(location: location);
+              },
+              onPageChanged: (index) => setState(() => pageIndex = index),
+            ),
+          ),
+          Text(
+            '${pageIndex + 1}/${locations.length}',
+            style: TextStyle(color: Colors.white70),
+          ),
+          SizedBox(height: 12)
+        ],
+      );
+}
